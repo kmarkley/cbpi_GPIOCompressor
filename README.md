15dadeba1351b0abac037ad07bdc4518553345fe
kmarkley <kirk@kirkmarkleydesign.com>
2020-02-29 12:22:18 -0600

    update __init__.py

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 # GPIOCompressor for CraftBeerPi
 
-This plugin adds Actors for controlling compressors (like refridgerators/freezers). To prevent damage to the compressor, you should not turn the compressor on and off repeatedly. It's best to have a delay between cycles. This actor allows for that delay.
+This plugin adds Actors for controlling compressors (like refrigerators/freezers). To prevent damage to the compressor, you should not turn the compressor on and off repeatedly. It's best to have a delay between cycles. This actor allows for that delay.
 
 There are two types of compressor
-- GPIOCompressor: A standard GPIO output compressoror
+- GPIOCompressor: A standard GPIO output compressor
 - RelayCompressor: A GPIO compressor with inverted output
 
 Both accept a number of minutes to delay before allowing turning the compressor back on again. The switch will display as though it is on, but the GPIO will not trigger unless enough time has elapsed.
 
-Future Features
-- Trigger GPIO if state is still "on" and wait time has elapsed
+Additionally, a duty cycle may be set to prevent the compressor from running continuously when executing large temperature changes.  Set the number of minutes the compressor should run and rest each cycle. As with the delay, CBPI will show the actor as on even when the compressor is off during the rest portion of the duty cycle.
--- __init__.py
+++ __init__.py
@@ -7,35 +7,117 @@ cbpi.gpio_compressors = []
 
 class Compressor(object):
     c_delay = Property.Number("Compressor Delay", True, 10, "minutes")
-    compressor_on = False
-    compressor_wait = datetime.utcnow()
-    delayed = False
+    d_cycle_on = Property.Number("Compressor Duty Cycle On", True, 180, "minutes")
+    e_cycle_off = Property.Number("Compressor Duty Cycle Off", True, 30, "minutes")
 
     def init(self):
         super(Compressor, self).init()
         cbpi.gpio_compressors += [self]
 
-    def on(self, power=0):
-        if datetime.utcnow() >= self.compressor_wait:
-            self.compressor_on = True
-            super(Compressor, self).on(power)
-            self.delayed = False
-        else:
-            print "Delaying Turing on Compressor"
-            self.delayed = True
+        self.actor_is_off = True
+        self.compressor_is_on = False
+        self.compressor_last_off = datetime(1,1,1)
+
+        self.delayed = False
+        self.delayed_on_time = datetime.now()
+
+        self.cycle_rest = False
+        self.cycle_on_time = datetime.now()
+        self.cycle_off_time = self.cycle_on_time + timedelta(minutes=int(self.d_cycle_on))
+
+        cbpi.app.logger.info("GPIOCompressor: '{}' initialized".format(self.name))
+
+
+    def on(self, power=100):
+        # actor was just turned on
+        if self.actor_is_off:
+            # set flag so this only executes once
+            self.actor_is_off = False
+            # reset active duty cycle
+            if not self.cycle_rest:
+                next_on = max(datetime.now(),self.delayed_on_time)
+                if next_on >= self.compressor_last_off + timedelta(minutes=int(self.e_cycle_off)):
+                    # if the actor has been off longer than the duty cycle rest, then reset the duty cycle
+                    self.cycle_off_time = next_on + timedelta(minutes=int(self.d_cycle_on))
+                else:
+                    # otherwise, add the time spent turned off to the current duty cycle
+                    self.cycle_off_time = self.cycle_off_time + (next_on - self.compressor_last_off)
+
+        self.automatic(power)
+
 
     def off(self):
-        if self.compressor_on:
-            self.compressor_on = False
-            self.compressor_wait = datetime.utcnow() + timedelta(minutes=int(self.c_delay))
-        self.delayed = False
+        # actor was just turned off
+        if not self.actor_is_off:
+            # set flag so this only executes once
+            self.actor_is_off = True
+            # set dalayed state
+            self.delayed = True
+            # determine when the delay ends
+            self.delayed_on_time = datetime.now() + timedelta(minutes=int(self.c_delay))
+            # write to log
+            cbpi.app.logger.info("GPIOCompressor: '{}' delayed until {}".format(self.name, self.delayed_on_time))
+
+        self.automatic()
+
+
+    def automatic(self, power=100):
+
+        # check if delay has expired
+        if self.delayed:
+            if datetime.now() >= self.delayed_on_time:
+                # unset delayed state
+                self.delayed = False
+                # write to log
+                cbpi.app.logger.info("GPIOCompressor: '{}' delay ended".format(self.name))
+
+        # check if the rest duty cycle has expired
+        if self.cycle_rest:
+            if datetime.now() >= self.cycle_on_time:
+                # unset rest state
+                self.cycle_rest = False
+                # write to log
+                cbpi.app.logger.info("GPIOCompressor: '{}' duty cycle active until {}".format(self.name, self.cycle_off_time))
+
+        # check if the active duty cycle has expired
+        else:
+            if datetime.now() >= self.cycle_off_time:
+                # set rest state
+                self.cycle_rest = True
+                # determine when the rest cycle ends
+                self.cycle_on_time = datetime.now() + timedelta(minutes=int(self.e_cycle_off))
+                # detrmine when the next active cycle ends (overwritten if actor is turned off/on)
+                self.cycle_off_time = self.cycle_on_time + timedelta(minutes=int(self.d_cycle_on))
+                # write to log
+                cbpi.app.logger.info("GPIOCompressor: '{}' duty cycle rest until {}".format(self.name, self.cycle_on_time))
+
+        # act on compressor
+        # cbpi.app.logger.info("GPIOCompressor: '{}' off:{} delayed:{} rest:{}".format(self.name, self.actor_is_off, self.delayed, self.cycle_rest))
+        if self.actor_is_off or self.delayed or self.cycle_rest:
+            self.compressor_off()
+        else:
+            self.compressor_on(power)
+
+
+    def compressor_on(self, power=100):
+        if not self.compressor_is_on:
+            self.compressor_is_on = True
+            cbpi.app.logger.info("GPIOCompressor: '{}' compressor now running".format(self.name))
+        super(Compressor, self).on(power)
+
+
+    def compressor_off(self):
+        if self.compressor_is_on:
+            self.compressor_is_on = False
+            self.compressor_last_off = datetime.now()
+            cbpi.app.logger.info("GPIOCompressor: '{}' compressor now off".format(self.name))
         super(Compressor, self).off()
+
 
 @cbpi.backgroundtask(key="update_compressors", interval=5)
 def update_compressors(api):
     for compressor in cbpi.gpio_compressors:
-        if compressor.delayed and datetime.utcnow() >= compressor.compressor_wait:
-            compressor.on()
+        compressor.automatic()
 
 @cbpi.actor
 class RelayCompressor(Compressor, RelayBoard):
